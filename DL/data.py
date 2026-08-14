e7c6534613a3ada402c04da71ebc0e14855e0acb
Юзеф Заборовский <Yuzef@MacBook-Air-Ola.local>
2026-08-14 22:07:46 +0300

    wip: dl pipeline.:

--- DL/data.py
+++ DL/data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sympy.logic.inference import valid
 import torch
 
 from sklearn.pipeline import Pipeline
@@ -82,3 +83,59 @@ def prepare_tuning_split(
     train_indices, valid_indices = cv_splits[0]
 
     feature_pipeline = build_dl_feature_pipeline(config)
+
+    # Разделение исходного X согласно индексам, которые дал CV.
+    X_train_raw = X.iloc[train_indices]
+    X_valid_raw = X.iloc[valid_indices]
+    
+    X_train = feature_pipeline.fit_transform(X_train_raw)
+    X_valid = feature_pipeline.transform(X_valid_raw)
+
+    X_train = to_float32_array(X_train)
+    X_valid = to_float32_array(X_valid)
+
+    y_log = transform_target(y)
+
+    y_train = y_log[train_indices]
+    y_valid = y_log[valid_indices]
+
+    return {
+        "X_train": X_train,
+        "X_valid": X_valid,
+        "y_train": y_train,
+        "y_valid": y_valid,
+        "train_indices": train_indices,
+        "valid_indices": valid_indices,
+        "feature_pipeline": feature_pipeline,
+    }
+
+def build_train_loader(
+    X: np.ndarray,
+    y: np.ndarray,
+    batch_size: int,
+    shuffle: bool,
+    seed: int,
+    num_workers: int,
+    pin_memory: bool,
+    drop_last: bool
+):
+    feature_tensor = torch.from_numpy(X)
+    target_tensor = torch.from_numpy(y)
+
+    dataset = TensorDataset(feature_tensor, target_tensor)
+
+    generator = torch.Generator()
+    generator.manual_seed(seed)
+
+    loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        drop_last=drop_last,
+        generator=generator if shuffle else None,
+        persistent_workers=(num_workers>0)
+    )
+
+    return loader, generator
